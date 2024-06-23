02c3a457a48bbea91f52876c5c3ea05f5da5a718
mynameiskuun <mynameiskuun@gmail.com>
2024-06-24 01:08:22 +0900

    PS initial commit

---
+++ src/main/java/com/example/codingtest/CodingTestApplication.java
@@ -0,0 +1,10 @@
+package com.example.codingtest;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestApplication {
+    public static void main(String[] args) {
+
+    }
+}
