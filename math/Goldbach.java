a6b5393ba5ea70b4cd460d4c114fe1125d515e23
mynameiskuun <mynameiskuun@gmail.com>
2024-07-19 01:33:04 +0900

    modified goldbach update

--- math/Goldbach.java
+++ math/Goldbach.java
@@ -3,34 +3,42 @@ package math;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Goldbach {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int MAX = 1000000;
+        boolean[] isPrime = new boolean[MAX + 1];
+        Arrays.fill(isPrime, true);
 
-        boolean[] isPrime = new boolean[1000001];
-        isPrime[0] = true;
-        isPrime[1] = true;
-        for (int i = 2; i <= (int) Math.sqrt(1000000); i++) {
-            for (int j = 2; i * j < 1000001; j++) {
-                isPrime[i * j] = true;
+        for(int i = 2; i <= MAX; i++) {
+            for(int j = i * 2; j <= MAX; j += i) {
+                if(!isPrime[j]) {
+                    continue;
+                }
+                isPrime[j] = false;
             }
         }
 
-        while (true) {
-            int n = Integer.parseInt(in.readLine());
-            if (n == 0) {
+        while(true) {
+            int targetNumber = Integer.valueOf(br.readLine());
+
+            if(targetNumber == 0) {
                 break;
             }
-            boolean ans = false;
-            for (int i = 2; i <= n / 2; i++) {
-                if (!isPrime[i] && !isPrime[n - i]) {
-                    System.out.println(n + " = " + i + " + " + (n - i));
-                    ans = true;
+
+            boolean flag = false;
+            for(int i=2; i<=targetNumber / 2; i++) {
+
+                    if(isPrime[i] && isPrime[targetNumber - i]) {
+                    System.out.println(targetNumber + " = " + i + " + " + (targetNumber - i));
+                    flag = true;
                     break;
                 }
             }
-            if (ans == false) {
+
+            if(!flag) {
                 System.out.println("Goldbach's conjecture is wrong.");
             }
         }
